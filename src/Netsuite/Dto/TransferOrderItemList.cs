38e59923c2d03c6c3cce958f7ad15b0e469bbafc
Ellis Rygg <ellis@dataninja.com>
2023-09-22 11:37:29 -0600

    Revert "remove unused transferOrderItem and transferOrderItemList"
    This reverts commit 42f3919fd4c37ed10af357d05b026d1ca6b28158.

---
+++ src/Netsuite/Dto/TransferOrderItemList.cs
@@ -0,0 +1,42 @@
+namespace Netsuite.Dto
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("wsdl", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="urn:inventory_2021_1.transactions.webservices.netsuite.com")]
+    public partial class TransferOrderItemList {
+
+        private TransferOrderItem[] itemField;
+
+        private bool replaceAllField;
+
+        public TransferOrderItemList() {
+            this.replaceAllField = true;
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("item")]
+        public TransferOrderItem[] item {
+            get {
+                return this.itemField;
+            }
+            set {
+                this.itemField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        [System.ComponentModel.DefaultValueAttribute(true)]
+        public bool replaceAll {
+            get {
+                return this.replaceAllField;
+            }
+            set {
+                this.replaceAllField = value;
+            }
+        }
+    }
+}
