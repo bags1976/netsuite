eb59f4859ac5b0d4df7973d70275a9f6218716d8
Joshua Bagnato <jbagnato@dataninja.com>
2023-12-07 15:46:24 -0700

    Update classification and delete records

--- src/Netsuite/Dto/Classification.cs
+++ src/Netsuite/Dto/Classification.cs
@@ -1,149 +1,16 @@
+using System;
+
 namespace Netsuite.Dto
 {
-    /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("wsdl", "4.8.3928.0")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(Namespace="urn:accounting_2021_1.lists.webservices.netsuite.com")]
-    public partial class Classification : Record {
-
-        private string nameField;
-
-        private bool includeChildrenField;
-
-        private bool includeChildrenFieldSpecified;
-
-        private RecordRef parentField;
-
-        private bool isInactiveField;
-
-        private bool isInactiveFieldSpecified;
-
-        private ClassTranslationList classTranslationListField;
-
-        private RecordRef[] subsidiaryListField;
-
-        private CustomFieldRef[] customFieldListField;
-
-        private string internalIdField;
-
-        private string externalIdField;
-
-        /// <remarks/>
-        public string name {
-            get {
-                return this.nameField;
-            }
-            set {
-                this.nameField = value;
-            }
-        }
-
-        /// <remarks/>
-        public bool includeChildren {
-            get {
-                return this.includeChildrenField;
-            }
-            set {
-                this.includeChildrenField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public bool includeChildrenSpecified {
-            get {
-                return this.includeChildrenFieldSpecified;
-            }
-            set {
-                this.includeChildrenFieldSpecified = value;
-            }
-        }
-
-        /// <remarks/>
-        public RecordRef parent {
-            get {
-                return this.parentField;
-            }
-            set {
-                this.parentField = value;
-            }
-        }
-
-        /// <remarks/>
-        public bool isInactive {
-            get {
-                return this.isInactiveField;
-            }
-            set {
-                this.isInactiveField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public bool isInactiveSpecified {
-            get {
-                return this.isInactiveFieldSpecified;
-            }
-            set {
-                this.isInactiveFieldSpecified = value;
-            }
-        }
-
-        /// <remarks/>
-        public ClassTranslationList classTranslationList {
-            get {
-                return this.classTranslationListField;
-            }
-            set {
-                this.classTranslationListField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlArrayItemAttribute("recordRef", Namespace="urn:core_2021_1.platform.webservices.netsuite.com", IsNullable=false)]
-        public RecordRef[] subsidiaryList {
-            get {
-                return this.subsidiaryListField;
-            }
-            set {
-                this.subsidiaryListField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlArrayItemAttribute("customField", Namespace="urn:core_2021_1.platform.webservices.netsuite.com", IsNullable=false)]
-        public CustomFieldRef[] customFieldList {
-            get {
-                return this.customFieldListField;
-            }
-            set {
-                this.customFieldListField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlAttributeAttribute()]
-        public string internalId {
-            get {
-                return this.internalIdField;
-            }
-            set {
-                this.internalIdField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlAttributeAttribute()]
-        public string externalId {
-            get {
-                return this.externalIdField;
-            }
-            set {
-                this.externalIdField = value;
-            }
-        }
+    public class Classification : Record
+    {
+        public bool IsInactive { get; set; }
+        public string Parent { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string FullName { get; set; }
+        public string Name { get; set; }
+        public string ExternalId { get; set; }
+        public bool IncludeChildren { get; set; }
+        public string Subsidiary { get; set; }
     }
 }
--- src/Netsuite/Dto/DeletedRecord.cs
+++ src/Netsuite/Dto/DeletedRecord.cs
@@ -1,48 +1,19 @@
+using System;
+
 namespace Netsuite.Dto
 {
-    /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("wsdl", "4.8.3928.0")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(Namespace="urn:core_2021_1.platform.webservices.netsuite.com")]
-    public partial class DeletedRecord {
-
-        private System.DateTime deletedDateField;
-
-        private bool deletedDateFieldSpecified;
-
-        private BaseRef recordField;
-
-        /// <remarks/>
-        public System.DateTime deletedDate {
-            get {
-                return this.deletedDateField;
-            }
-            set {
-                this.deletedDateField = value;
-            }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public bool deletedDateSpecified {
-            get {
-                return this.deletedDateFieldSpecified;
-            }
-            set {
-                this.deletedDateFieldSpecified = value;
-            }
-        }
-
-        /// <remarks/>
-        public BaseRef record {
-            get {
-                return this.recordField;
-            }
-            set {
-                this.recordField = value;
-            }
-        }
+    public class DeletedRecord
+    {
+        public string ScriptId { get; set; }
+        public string DeletedBy { get; set; }
+        public bool IsCustomList { get; set; }
+        public string RecordId { get; set; }
+        public bool IsCustomRecord { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public bool IsCustomTransaction { get; set; }
+        public string RecordTypeId { get; set; }
+        public string Context { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
     }
 }
