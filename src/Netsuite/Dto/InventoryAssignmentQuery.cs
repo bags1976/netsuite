0774b3ab51ebb15382646b59809d341a65f68b3d
Joshua Bagnato <jbagnato@dataninja.com>
2025-04-08 15:40:37 -0600

    Add assignment DOE

--- src/Netsuite/Dto/InventoryAssignmentQuery.cs
+++ src/Netsuite/Dto/InventoryAssignmentQuery.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace Netsuite.Dto
 {
     public class InventoryAssignmentQuery
     {
         public decimal ReservedQuantity { get; set; }
         public string InventoryNumber { get; set; }
+        public DateTime? ExpirationDate { get; set; }
     }
 }
